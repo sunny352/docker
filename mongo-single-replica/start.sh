879517433792100428e80aa14cd2236985786b96
MingSailor <sunny352@users.noreply.github.com>
2024-08-30 16:07:38 +0800

    修改start.sh启动脚本的实现

--- mongo-single-replica/start.sh
+++ mongo-single-replica/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ip=$(hostname -I | awk '{print $1}')
+echo "当前主机IP: $ip"
+
 #如果不存在mongo-keyfile文件，则创建
 if [ ! -f mongo-keyfile ]; then
     echo "找不到mongo-keyfile文件，创建中..."
@@ -14,8 +17,13 @@ docker compose up -d
 
 #等待mongo启动
 echo "等待mongo启动"
-sleep 10
+for i in {1..300}; do
+    docker exec -it mongo-rs0 mongo --eval "db.adminCommand('ping')" > /dev/null 2>&1 && break
+    sleep 1
+done
 
 #初始化mongo
 echo "初始化mongo"
-docker exec -it mongo-rs0 mongo --authenticationDatabase admin -u root -p 123456 --eval "rs.initiate({_id: 'rs0', members: [{_id: 0, host: '10.0.68.124:27017'}]})"
+script="rs.initiate({_id: 'rs0', members: [{_id: 0, host: '$ip:27017'}]})"
+echo "执行脚本: $script"
+docker exec -it mongo-rs0 mongosh --authenticationDatabase admin -u root -p 123456 --eval "$script"
