509e378c3f5b40d603b0073836aa781f071cd2e5
MingSailor <sunny352@users.noreply.github.com>
2024-12-25 17:14:22 +0800

    移除docker-compose.yml，集成到start.sh中，并添加port和集群名的配置

--- mongo-single-replica/README.md
+++ mongo-single-replica/README.md
@@ -10,11 +10,10 @@
 
 ## 使用方式
 
-* 启动：运行start.sh
+* 启动：运行start.sh，默认端口27017，集群名为rs0，可在start.sh中修改
 * 停止：运行stop.sh
 
 ## 说明
 
-* docker-compose.yml中定义了一个mongo单节点副本集
-* start.sh脚本会创建mongo的集群key文件、启动docker compose集群并且初始化集群
+* start.sh脚本会创建mongo的集群key文件、创建并启动docker compose集群并且初始化集群
 * stop.sh脚本会停止docker compose集群
--- mongo-single-replica/start.sh
+++ mongo-single-replica/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+port=27017
+replSetName=rs0
 ip=$(hostname -I | awk '{print $1}')
+
 echo "当前主机IP: $ip"
+echo "当前端口: $port"
+echo "当前副本集名称: $replSetName"
 
 #如果不存在mongo-keyfile文件，则创建
 if [ ! -f mongo-keyfile ]; then
@@ -11,6 +16,31 @@ if [ ! -f mongo-keyfile ]; then
     chown 999:999 mongo-keyfile
 fi
 
+#如果不存在docker-compose.yml文件，则创建
+if [ ! -f docker-compose.yml ]; then
+    echo "找不到docker-compose.yml文件，创建中..."
+    cat > docker-compose.yml << EOL
+version: '3'
+services:
+    mongo:
+        image: mongo:5.0 #需要MongoDB 5.0以上版本
+        container_name: mongo-${replSetName}
+        ports:
+            - "${port}:27017"
+        expose:
+            - "27017"
+        restart: always
+        environment:
+            MONGO_INITDB_ROOT_USERNAME: "root"
+            MONGO_INITDB_ROOT_PASSWORD: "123456"
+            TZ: "Asia/Shanghai"
+        volumes:
+            - "./mongo:/data/db"
+            - "./mongo-keyfile:/etc/mongo-keyfile:ro"
+        command: [ "mongod", "--replSet", "${replSetName}", "--keyFile", "/etc/mongo-keyfile" ]
+EOL
+fi
+
 #启动docker-compose
 echo "启动docker-compose"
 docker compose up -d
@@ -18,12 +48,12 @@ docker compose up -d
 #等待mongo启动
 echo "等待mongo启动"
 for i in {1..300}; do
-    docker exec -it mongo-rs0 mongo --eval "db.adminCommand('ping')" > /dev/null 2>&1 && break
+    docker exec -it mongo-${replSetName} mongo --eval "db.adminCommand('ping')" > /dev/null 2>&1 && break
     sleep 1
 done
 
 #初始化mongo
 echo "初始化mongo"
-script="rs.initiate({_id: 'rs0', members: [{_id: 0, host: '$ip:27017'}]})"
+script="rs.initiate({_id: '${replSetName}', members: [{_id: 0, host: '$ip:$port'}]})"
 echo "执行脚本: $script"
-docker exec -it mongo-rs0 mongosh --authenticationDatabase admin -u root -p 123456 --eval "$script"
+docker exec -it mongo-${replSetName} mongosh --authenticationDatabase admin -u root -p 123456 --host $ip --eval "$script"
